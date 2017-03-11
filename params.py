51ccf134bd294ce12cbf4e2b841493e9c0a4270c
Curtis H <CurtLHampton@gmail.com>
2017-03-11 13:37:47 -0600

    add documentation

--- params.py
+++ params.py
@@ -1,4 +1,6 @@
+# edit this list of categories to collect for each city
 categories = set(['TherapeuticMassage', 'WomenSeekMen'])
 
+# visit http://www.backpage.com/ for a list of potential cities
 cities = set(['alexandria', 'batonrouge', 'houma', 'lafayette', 'lakecharles',
               'monroe', 'neworleans', 'shreveport'])
