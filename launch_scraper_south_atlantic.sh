52a0318bc4f457e588bb57a2a07dca0404f1b814
Curtis <CurtLHampton@gmail.com>
2017-05-04 20:30:41 -0500

    remove old files

--- launch_scraper_south_atlantic.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-cd /home/elmer/frontPage
-
-python scraper.py --city_file cities_south_atlantic.txt
-
--- launch_scraper_west_south_central.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-cd /home/elmer/frontPage
-
-python scraper.py --city_file cities_west_south_central.txt
-
